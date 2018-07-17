e1f20371d9f2d7523f0af5112f70a81e13634c12
Rj <remy.jell@concordiatec.com>
2018-07-17 14:27:18 +0900

    Modify erro image src

--- public/html/common/header.php
+++ public/html/common/header.php
@@ -92,26 +92,26 @@
                     <!-- END USER LOGIN DROPDOWN -->
                     <li class="dropdown dropdown-language">
                         <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                            <img alt="" src="../assets/global/img/flags/us.png">
+                            <img alt="" src="/img/flags/us.png">
                             <span class="langname"> US </span>
                             <i class="fa fa-angle-down"></i>
                         </a>
                         <ul class="dropdown-menu">
                             <li>
                                 <a href="javascript:;">
-                                    <img alt="" src="../assets/global/img/flags/es.png"> Spanish </a>
+                                    <img alt="" src="/img/flags/es.png"> Spanish </a>
                             </li>
                             <li>
                                 <a href="javascript:;">
-                                    <img alt="" src="../assets/global/img/flags/de.png"> German </a>
+                                    <img alt="" src="/img/flags/de.png"> German </a>
                             </li>
                             <li>
                                 <a href="javascript:;">
-                                    <img alt="" src="../assets/global/img/flags/ru.png"> Russian </a>
+                                    <img alt="" src="/img/flags/ru.png"> Russian </a>
                             </li>
                             <li>
                                 <a href="javascript:;">
-                                    <img alt="" src="../assets/global/img/flags/fr.png"> French </a>
+                                    <img alt="" src="/img/flags/fr.png"> French </a>
                             </li>
                         </ul>
                     </li>
