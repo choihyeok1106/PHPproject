735aa8bff86ef2c43d2d6da060c5483ccda849ff
Rj <remy.jell@concordiatec.com>
2018-12-24 18:06:05 +0900

    Add X-App-Locale in Service::header

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,6 @@
 namespace App\Http\Controllers;
 
 
-use App\Cache\Cache;
-use App\Cache\RankCache;
-use App\Models\HomeInterface;
-use App\Repositories\Rep;
-use App\Services\AuthenticateService;
-use App\Services\NewsService;
-use App\Services\RankService;
-use App\Services\RepService;
-use App\Supports\UserPrefs;
-
 class HomeController extends Controller {
 
     public function __construct() {
